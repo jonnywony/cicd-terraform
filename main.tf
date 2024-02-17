22043b33eacd30e08ae1932c6a5f7f0bc682dfdb
Jonathan Chan <jonathanchanwj@gmail.com>
2024-02-17 12:20:09 +0800

    separate files

--- main.tf
+++ main.tf
@@ -1,16 +1,11 @@
-terraform {
-  backend "s3" {
-    bucket = "sctp-ce5-tfstate-bucket-1"
-    key    = "jon.tfstate"
-    region = "us-east-1"
-  }
+
+locals {
+  topic-name = "updates-topic"
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
+
 
 #new sns topic
 resource "aws_sns_topic" "user_updates" {
-  name = "jonathan-c-updates-topic"
+  name = local.topic-name
 }
---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable abc {
+  type        = string
+  default     = "jon-c-updates-topic"
+  description = "this is a type"
+}
