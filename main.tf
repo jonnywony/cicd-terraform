28efd148db4821d5e5df10d1f71a3bb1f8802470
Jonathan Chan <jonathanchanwj@gmail.com>
2024-02-17 12:39:54 +0800

    new secret

--- main.tf
+++ main.tf
@@ -3,10 +3,21 @@ locals {
   topic-name = "updates-topic"
 }
 
-
+# add this, then move it to variables.tf
+variable abc {
+  type        = string
+  default     = "jon-c-updates-topic"
+  description = "this is a type"
+}
 
 #new sns topic
 resource "aws_sns_topic" "user_updates" {
   # change this
   name = var.abc
 }
+
+
+# add this, then move it to environments/dev.tfvars
+abc = "dev"
+# add this, then move it to environments/prod.tfvars
+abc = "prod"
