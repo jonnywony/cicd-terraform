511c1edcf0ae3491d0c1cc3087316f8045025bdc
jonnywony <49757046+jonnywony@users.noreply.github.com>
2024-09-02 19:32:45 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 # add this, then move it to variables.tf
 variable abc {
   type        = string
-  default     = "jon-c-updates-topic"
+  default     = "jon-c-updates-topics"
   description = "this is a type"
 }
