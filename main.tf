258befb10b4ed69d4bd6c8cd6ee3f9721f307dc4
Jonathan Chan <jonathanchanwj@gmail.com>
2024-09-04 20:35:14 +0800

    tmy first tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 #new sns topic
 resource "aws_sns_topic" "user_updates" {
   # change this
-     //name = "${var.abc}-sns-topic"
-     name = "jon-sns-topic"
+     name = "${var.abc}-sns-topic"
+     //name = "jon-sns-topic"
+      //name = "${local.topic-name}-sns-topic"
 }
